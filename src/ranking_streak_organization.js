c5600aabe797c74d4a437284535fc858f7969d39
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-11-04 03:12:58 +0900

    feat: 정렬 로직 최적화

--- src/ranking_streak_organization.js
+++ src/ranking_streak_organization.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
-const OPTIONS = {
-  method: "GET",
-  url: "https://solved.ac/api/v3/ranking/arena_in_organization",
-  params: { organizationId: "436" },
-  headers: { Accept: "application/json" },
-};
+const ORGANIZATION_ID = 436; /** 알아내고 싶은 단체의 ID를 입력함. */
+const PAGES = 19; /** 단체의 랭킹 페이지를 보고, 최대 페이지 수를 확인함. */
 
 let ranks = [];
 
-for (let i = 1; i < 20; i++) {
+for (let page = 0; page < PAGES; page++) {
   const options = {
     method: "GET",
     url: "https://solved.ac/api/v3/ranking/arena_in_organization",
-    params: { organizationId: "436", page: i },
+    params: { organizationId: ORGANIZATION_ID, page: page + 1 },
     headers: { Accept: "application/json" },
   };
 
@@ -27,10 +23,10 @@ for (let i = 1; i < 20; i++) {
     throw new Error("에러");
   }
 }
-ranks.push([2024, "PASS"]);
+
 ranks.sort((a, b) => b[0] - a[0]);
 
 for (let each of ranks.entries()) {
   if (each[1][1] == "PASS") continue;
-  else console.log(`${each[0]}등\t${each[1][0]}일\t${each[1][1]}님`);
+  else console.log(`${each[0] + 1}등\t${each[1][0]}일\t${each[1][1]}님`);
 }
